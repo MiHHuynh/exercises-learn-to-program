e270f1b110a200df7d057adc9cb265a71cd3794f
Michelle Huynh <michehuynh@yahoo.com>
2017-02-20 21:38:13 -0800

    Ch 6 - Angry Boss

--- Chapter6/angry_boss.rb
+++ Chapter6/angry_boss.rb
@@ -1,1 +1,8 @@
-help
+# Write an angry boss program that rudely asks what you want.
+# Whatever you answer, the angry boss should yell it back to you
+# and then fire you. For example, if you type in "I want a raise,"
+# it should yell back, "WHADDAYA MEAN 'I WANT A RAISE'?!? YOU'RE FIRED!"
+
+puts "What do you want?!"
+request = gets.chomp
+puts "WHADDAYA MEAN " + "\"" + request.upcase + "\"?!? YOU'RE FIRED!"
