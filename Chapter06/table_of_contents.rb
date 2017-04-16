17eda2f0f6f2bc4fadd6219ed63c47cfcbc73498
Michelle Huynh <michehuynh@yahoo.com>
2017-04-16 14:13:14 -0700

    added code for table_of_contents exercise

---
+++ Chapter06/table_of_contents.rb
@@ -0,0 +1,11 @@
+# Here's something for you to do in order to play around more with center, ljust, and rjust:
+# Write a program that will display a table of contents so that it looks like this:
+# Chapter 1:    Getting Started				page   1
+# Chapter 2:    Numbers						page   9
+# Chapter 3:    Letters						page  13
+
+line_width = 50
+puts('Table of Contents'.center(line_width))
+puts('Chapter 1:    Getting Started'.ljust(30) + 'page   1'.rjust(20))
+puts('Chapter 2:    Numbers'.ljust(30) + 'page   9'.rjust(20))
+puts('Chapter 3:    Letters'.ljust(30) + 'page  13'.rjust(20))
