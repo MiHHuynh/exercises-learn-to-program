f7ded1efef930567a7712800583a574cec66be39
Michelle Huynh <michehuynh@yahoo.com>
2017-04-18 00:54:55 -0700

    added comments/descriptions for modern_roman_numerals and old_school_roman_numerals

---
+++ Chapter09/modern_roman_numerals.rb
@@ -0,0 +1,7 @@
+# Write a method that when passed an integer between 1 and 3000
+# returns a string containing the new Roman numeral with subtraction.
+# e.g. modern_roman_numeral 4 should return IV.
+
+# For Reference:
+# I = 1		V = 5		X = 10		L = 50		C = 100		D = 500		M = 1000
+
